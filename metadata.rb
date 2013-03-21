1014d68585ccdbcbfee322d9fe5a8e1e9e856a37
Jim <jarosser06@gmail.com>
2013-03-21 18:39:54 -0400

    upped version number

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'jarosser06@arch.tamu.edu'
 license          'MIT'
 description      'Installs/Configures memcached'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '13.3.2'
+version          '13.3.3'
