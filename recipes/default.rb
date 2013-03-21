6b8464fca8a89505440e5257d58f004657a73539
Jim <jarosser06@gmail.com>
2013-03-21 18:39:36 -0400

    fixed food critic warnings

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,23 +24,23 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-package node["memcached"]["package"] do
+package node[:memcached][:package] do
   action [:install]
 end
 
 # Allows for memory percentage to be used
-if node["memcached"]["max_mem"].to_s.match("%")
-  node.set[:memcached][:max_mem]= ((node["memory"]["total"].to_i / 1024)*(node["memcached"]["max_mem"].to_i * 0.01)).to_i
+if node[:memcached][:max_mem].to_s.match("%")
+  node.set[:memcached][:max_mem]= ((node[:memory][:total].to_i / 1024)*(node[:memcached][:max_mem].to_i * 0.01)).to_i
 end
 
-template node["memcached"]["config"] do
+template node[:memcached][:config] do
   source "memcached.conf.erb"
   mode "0644"
   owner "root"
   group "root"
-  notifies :restart, "service[#{node["memcached"]["service_name"]}]"
+  notifies :restart, "service[#{node[:memcached][:service_name]}]"
 end 
 
-service node["memcached"]["service_name"] do
+service node[:memcached][:service_name] do
   action [:enable, :start]
 end
